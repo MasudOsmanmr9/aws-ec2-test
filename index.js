a0f7b71e32226d590e050ebd51663ddc73c4b20c
masud osman <masudosmanmr9@gmail.com>
2023-09-02 03:28:32 +0600

    user route added

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/user', (req, res) => {
+    res.json({
+        'name':'masud',
+        'age':29,
+        'height':5.9,
+        'gpa':2.9
+    })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
